ec4f51cc0cd1bfda3c342feca44fadc3470707b0
Aaron Berns <anberns@gmail.com>
2019-01-20 18:53:24 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,7 +3,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT characters.name, MAX(length FROM characters GROUP BY characters.motto.length AS length"
+  "SELECT characters.name, MAX(length) FROM characters GROUP BY characters.motto.length AS length"
 end
 
 
