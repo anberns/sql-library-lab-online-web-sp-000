3916c78b5d29453eed3bbca0536a96c884e68eea
Aaron Berns <anberns@gmail.com>
2019-01-20 20:07:56 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -7,7 +7,7 @@ def select_name_and_motto_of_char_with_longest_motto
 end
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters.species, COUNT(characters.species) as spec_count FROM characters GROUP BY characters.species HAVING (SELECT max(spec_count))"
+  "SELECT characters.species, MAX(spec_count) FROM characters GROUP BY characters.species HAVING (SELECT COUNT(characters.species) as spec_count)"
 end
 
 def select_name_and_series_subgenres_of_authors
